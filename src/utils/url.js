ff7a9d253c07d60be205f966d74af4e438ec5ff1
Péricles Pires <periclespires@protonmail.com>
2023-01-04 11:50:45 -0300

    verifying url via regex

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,11 +1,7 @@
 function isValidHttpUrl(string) {
-    let url;
-    try {
-      url = new URL(string);
-    } catch (_) {
-      return false;
-    }
-    return url.protocol === "http:" || url.protocol === "https:";
+    let regex = RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi);
+
+    return !!string.match(regex);
 }
 
 module.exports = { isValidHttpUrl }
